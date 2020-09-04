4d231e5576f343c505e895ea93810f03d8741774
Anne Nardolilli <anardolilli@gmail.com>
2020-09-04 15:00:17 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,26 +3,3 @@ def addition(num1, num2)
   puts sum 
 end
 
-def subtraction(num1, num2)
-  difference = num1 - num2
-  puts difference 
-end
-
-def division(num1, num2)
-  quotient = num1 / num2
-  puts quotient 
-end
-
-def multiplication(num1, num2)
-  product = num1 * num2
-  puts product 
-end
-
-def modulo(num1, num2)
-  remainder = num1 % num2
-  puts remainder 
-end
-
-def square_root(num)
-  root = 
-end
